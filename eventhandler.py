5c3bb1ce26cb827a6216b4d69e3e7f7e61195f48
IsaiahSama <zeldaplayergl@gmail.com>
2021-08-31 21:29:52 -0300

    No longer handles setting screen color

--- eventhandler.py
+++ eventhandler.py
@@ -3,7 +3,6 @@ from pygame import event, QUIT
 
 class EventHandler:
     def __init__(self, screen):
-        self.screen = screen
         self.screen_color = (194,178,128)
 
     def check_for_events(self):
@@ -11,10 +10,7 @@ class EventHandler:
             if task.type == QUIT:
                 raise SystemExit()
             if task.type == KEYDOWN:
-                self.handle_key_press(task.key)
-        
-        self.screen.fill(self.screen_color)
-        
+                self.handle_key_press(task.key)        
     
     def handle_key_press(self, key):
         if key == K_ESCAPE:
