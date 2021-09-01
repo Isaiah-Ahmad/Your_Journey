909e4dba514ad551e8a88a0664039ba52413f696
IsaiahSama <zeldaplayergl@gmail.com>
2021-08-31 22:41:02 -0300

    Finished setting up loading screen

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from pygame.constants import *
 from pygame.time import Clock
 
 # Created imports
-from sprites import Player
 from mechanics import mechs
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from eventhandler import EventHandler
@@ -12,28 +11,38 @@ from eventhandler import EventHandler
 
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Isaiah's Journey")
+
 clock = Clock()
 
-eventhandler = EventHandler(screen)
+eventhandler = EventHandler()
 
-mp = Player()
+# Set home screen
+mechs.setup_home_screen()
 
 # Sprite Groups
 all_sprites = pygame.sprite.Group()
-all_sprites.add(mp)
-
 cactus_sprites = pygame.sprite.Group()
 
 running = True
+playing = False
 while running:
+    # Setting the screen color
+    screen.fill(eventhandler.screen_color)
+    # Gotta make sure they start the game :D
+    if not playing:
+        screen = mechs.load_screen(screen)
+        eventhandler.check_for_events()
+        pygame.display.update()
+        continue
+
     # Gotta check for those events
     eventhandler.check_for_events()
-            
-    mp.update(pygame.key.get_pressed())
 
     # Load in sprites
     for sprite in all_sprites:
         screen.blit(sprite.surf, sprite.rect)
 
     pygame.display.flip()
+    pygame.display.update()
     clock.tick(30)
--- mechanics.py
+++ mechanics.py
@@ -1,9 +1,37 @@
-from sprites import Cactus
 from random import randint, sample
+from pygame.font import Font
+from pygame.image import load
+from pygame.transform import scale
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 
 class Mechanics:
     def __init__(self) -> None:
-        pass
+        self.gui = []
+        
+
+    def setup_home_screen(self):
+        # Title
+        title = scale(load("./Assets/images/text.png").convert(), (int(SCREEN_WIDTH * 0.7), int(SCREEN_HEIGHT * 0.7)))
+        title.set_colorkey((0, 0, 0))
+        title_rect = title.get_rect(center=((SCREEN_WIDTH // 2), int(SCREEN_HEIGHT * 0.2)))
+
+        sword = scale(load("./Assets/images/sword.png").convert(), (int(SCREEN_WIDTH * 0.3), int(SCREEN_HEIGHT * 0.7)))
+        sword.set_colorkey((0, 0, 0))
+        sword_rect = sword.get_rect(center=((SCREEN_WIDTH // 2), int(SCREEN_HEIGHT * 0.4)))
+        
+        start_font = Font('freesansbold.ttf', 35)
+        start_text = start_font.render("Press the ENTER key to begin", True, (200, 200, 200))
+        start_text_rect = start_text.get_rect(center=(int(SCREEN_WIDTH // 2), int(SCREEN_HEIGHT * 0.8)))
+        
+        self.gui.append((sword, sword_rect))
+        self.gui.append((title, title_rect))
+        self.gui.append((start_text, start_text_rect))
+
+    def load_screen(self, screen):
+        for gui in self.gui:
+            screen.blit(gui[0], gui[1])
+
+        return screen
 
     def should_generate(self, chance, max):
         valid = sample(range(max), k=chance)
