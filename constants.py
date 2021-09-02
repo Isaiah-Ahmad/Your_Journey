1860012cd8ae5320d769a4c8b159b17b2295a910
IsaiahSama <zeldaplayergl@gmail.com>
2021-09-02 01:35:00 -0300

    Now contains a list of gamestate constants

--- constants.py
+++ constants.py
@@ -3,3 +3,10 @@ from pyautogui import size
 screen_size = size()
 
 SCREEN_WIDTH, SCREEN_HEIGHT = int(screen_size[0]), int(screen_size[1])
+
+class GameState:
+    STARTED = 1
+    FREEPLAY = 2
+    SPEECH = 3 # Requires Mechs, Display, Screen, Dialogues
+
+gamestate = GameState()
