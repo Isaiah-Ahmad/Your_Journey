134bea03c0eeabe16669a1b0b10e1c7029494f74
IsaiahSama <zeldaplayergl@gmail.com>
2021-09-02 03:10:48 -0300

    Will now change terrain when attempting to go off screen

--- sprites.py
+++ sprites.py
@@ -2,6 +2,8 @@ import pygame.sprite as ps
 from pygame.image import load
 from pygame.transform import scale
 from pygame.locals import *
+from pygame.event import post, Event
+from eventhandler import TERRAINCHANGE
 from random import randint
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 
@@ -10,7 +12,7 @@ class Player(ps.Sprite):
         super(Player, self).__init__()
         self.surf = scale(load("./Assets/sprites/iseh2.png").convert(), (50, 50))
         self.surf.set_colorkey((255, 255, 255))
-        self.rect = self.surf.get_rect()
+        self.rect = self.surf.get_rect(center=(int(SCREEN_WIDTH // 2), int(SCREEN_HEIGHT // 2)))
 
     def update(self, pressed):
         if pressed[K_UP]:
@@ -24,13 +26,21 @@ class Player(ps.Sprite):
 
         # Keep player on the screen
         if self.rect.left < 0:
-            self.rect.left = 0
+            self.rect.right = SCREEN_WIDTH - 30
+            post(Event(TERRAINCHANGE, {}))
+
         if self.rect.right > SCREEN_WIDTH:
-            self.rect.right = SCREEN_WIDTH
+            self.rect.left = 30
+            post(Event(TERRAINCHANGE, {}))
+
         if self.rect.top <= 0:
-            self.rect.top = 0
+            self.rect.bottom = SCREEN_HEIGHT - 30
+            post(Event(TERRAINCHANGE, {}))
+
         if self.rect.bottom >= SCREEN_HEIGHT:
-            self.rect.bottom = SCREEN_HEIGHT
+            self.rect.top = 30
+            post(Event(TERRAINCHANGE, {}))
+
 
 class Cactus(ps.Sprite):
     def __init__(self):
